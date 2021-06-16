d6398e9939f12a7c5a2dd8441ced83e7b32560b9
Tsuneki Umeda <umedat@amazon.co.jp>
2021-06-16 20:42:48 +0900

    Spring config with code demo

---
+++ SportConfig.java
@@ -0,0 +1,22 @@
+package com.code.springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.code.springdemo")
+public class SportConfig {
+
+    //define bean for out sad fortune service
+    @Bean
+    public IFortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    //define bean for out swim coach AND inject dependency
+    @Bean
+    public ICoach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
+}
